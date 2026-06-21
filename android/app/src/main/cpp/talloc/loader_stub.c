155ce27c651cd2aa5882e23eace3e6561cfb6409
wisdom131-max <ijeziewisdom5@gmail.com>
2026-06-21 05:09:19 +0000

    fix: add missing loader_stub.c for proot build

--- android/app/src/main/cpp/talloc/loader_stub.c
+++ android/app/src/main/cpp/talloc/loader_stub.c
@@ -1,3 +1,10 @@
-char _binary_loader_exe_start[1] = {0};
-char _binary_loader_exe_end[1] = {0};
-unsigned long offset_to_pokedata_workaround = 0;
+#include <stdlib.h>
+#include <string.h>
+
+void *loader_malloc(size_t size) {
+    return malloc(size);
+}
+
+void loader_free(void *ptr) {
+    free(ptr);
+}
