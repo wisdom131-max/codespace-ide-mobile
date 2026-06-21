41a4f56e9254ff35d53725140e73bc50b0f67caf
wisdom131-max <ijeziewisdom5@gmail.com>
2026-06-21 05:57:52 +0000

    fix: add all missing linker stubs for proot build

--- android/app/src/main/cpp/talloc/loader_stub.c
+++ android/app/src/main/cpp/talloc/loader_stub.c
@@ -1,5 +1,13 @@
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+
+/* Stub loader binary symbols */
+char _binary_loader_exe_start[1] = {0};
+char _binary_loader_exe_end[1] = {0};
+
+/* Stub for pokedata workaround */
+uintptr_t offset_to_pokedata_workaround = 0;
 
 void *loader_malloc(size_t size) {
     return malloc(size);
